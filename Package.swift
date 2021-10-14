8c5d32f5e1207c7a2e4e221210606449bab4bad4
Domenic Bianchi <domenic@oncallhealth.com>
2021-10-14 12:08:57 -0400

    Save

--- Package.swift
+++ Package.swift
@@ -32,7 +32,7 @@ let package = Package(
             url: "https://github.com/gonzalezreal/NetworkImage",
             .exact("2.1.0")
         ),
-        .package(url: "https://github.com/pointfreeco/combine-schedulers", .exact("0.1.2")),
+        .package(url: "https://github.com/pointfreeco/combine-schedulers", .exact("0.5.1")),
         .package(
             name: "SnapshotTesting",
             url: "https://github.com/pointfreeco/swift-snapshot-testing",
